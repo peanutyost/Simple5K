2bb9925df69c7c933a260a49482b2511060c52d5
peanutyost <contact@netizenllc.net>
2024-05-24 15:05:38 -0500

    Fixed Static Files

--- Simple5K/Simple5K/settings.py
+++ Simple5K/Simple5K/settings.py
@@ -129,7 +129,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
